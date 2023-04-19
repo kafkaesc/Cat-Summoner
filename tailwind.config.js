73a2bdd3f8ec7cd891de3e6651a510b5d18fcb10
kafkaesc <jared.a.hettinger@gmail.com>
2023-04-19 15:38:36 -0500

    Adding colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
+		colors: {
+			black: '#000',
+			'cs-black': '#222',
+			'cs-blue': '#38678f',
+			'cs-green': '#53a135',
+			'cs-white': '#ddd',
+			red: '#f00',
+			white: '#fff',
+		},
 		extend: {},
 	},
 	plugins: [],
