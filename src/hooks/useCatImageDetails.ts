ce194d368fdf9893490eb22c3a85feaf6ca97efa
kafkaesc <jared.a.hettinger@gmail.com>
2023-05-22 20:22:45 -0500

    Making the cat image folder a const variable

--- src/hooks/useCatImageDetails.ts
+++ src/hooks/useCatImageDetails.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { CatImage } from '@/interfaces/CatImage';
 
 // TODO: use idLength to pad the src with zeros
+const catImageFolder = '/assets/images/cats/';
 const idLength = 2;
 
 const catImageData: Array<CatImage> = [
@@ -9,61 +10,61 @@ const catImageData: Array<CatImage> = [
 		alt: 'A black cat sits on a table with their two front legs covering a stack of cards. The logo is visible on the cards, revealing that the cat is interrupting a game of Exploding Kittens. The cat stares up into the camera with a deadpan expression, unconcerned with their interruption of the game.',
 		imageId: 1,
 		name: 'fearless',
-		src: '/assets/images/cats/fearless-01.jpg',
+		src: `${catImageFolder}fearless-01.jpg`,
 	},
 	{
 		alt: 'An orange cat sits on a gray bed sheet. They are sitting back, with their front legs facing forward and their back legs facing toward the camera. Their tail is wrapped around their back legs. Their ears are perked up and their eyes are squeezed near-shut in a pleasant expression.',
 		imageId: 1,
 		name: 'harvey',
-		src: '/assets/images/cats/harvey-01.jpg',
+		src: `${catImageFolder}harvey-01.jpg`,
 	},
 	{
 		alt: 'An orange cat sits on the hood of an electric-blue car. They lean slightly toward the camera with a curious look on their face.',
 		imageId: 2,
 		name: 'harvey',
-		src: '/assets/images/cats/harvey-02.jpg',
+		src: `${catImageFolder}harvey-02.jpg`,
 	},
 	{
 		alt: 'TODO: Allan please add the alt text for this image.',
 		imageId: 3,
 		name: 'harvey',
-		src: '/assets/images/cats/harvey-03.jpg',
+		src: `${catImageFolder}harvey-03.jpg`,
 	},
 	{
 		alt: 'A close-up head shot of an orange cat. Their face resembles a human face expressing irritation and annoyance.',
 		imageId: 4,
 		name: 'harvey',
-		src: '/assets/images/cats/harvey-04.jpg',
+		src: `${catImageFolder}harvey-04.jpg`,
 	},
 	{
 		alt: 'A mostly white cat with black spots around their eyes and on their back faces the camera. The cat is walking forward with their front right paw tucked under their body. Their black tail is held up with a hook at the top. Their eyes look into the camera with curiosity.',
 		imageId: 1,
 		name: 'lalo',
-		src: '/assets/images/cats/lalo-01.jpg',
+		src: `${catImageFolder}lalo-01.jpg`,
 	},
 	{
 		alt: 'A cat sits at the edge of a porch beneath a black night sky. Their fur is mostly white with some black spots down the back. The cat has their back to the camera, but their head is turned to face backward, making them seem peaceful and contemplative.',
 		imageId: 2,
 		name: 'lalo',
-		src: '/assets/images/cats/lalo-02.jpg',
+		src: `${catImageFolder}lalo-02.jpg`,
 	},
 	{
 		alt: 'A mostly white cat with black spots around their eyes lies inside of a cylindrical wicker enclosed bed. Their head peeks out of the circular hole on its front. Inside the enclosure the cat can be seen holding onto a stuffed chile pepper toy.',
 		imageId: 3,
 		name: 'lalo',
-		src: '/assets/images/cats/lalo-03.jpg',
+		src: `${catImageFolder}lalo-03.jpg`,
 	},
 	{
 		alt: 'A mostly white cat with black spots around their eyes and on their back sits on the edge of a blue porch that stretches off into the background. Their body is perpendicular to the camera with their tail wrapping forward, creating a classic cat silhouette.',
 		imageId: 4,
 		name: 'lalo',
-		src: '/assets/images/cats/lalo-04.jpg',
+		src: `${catImageFolder}lalo-04.jpg`,
 	},
 	{
 		alt: 'A mostly white cat with black spots around their eyes and on their back stands atop a roof. They are perched above the camera and face downward with their mouth open mid-meow. Behind them arcs a rainbow.',
 		imageId: 5,
 		name: 'lalo',
-		src: '/assets/images/cats/lalo-05.jpg',
+		src: `${catImageFolder}lalo-05.jpg`,
 	},
 ];
 
