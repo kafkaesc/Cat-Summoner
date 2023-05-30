75b274e53cb62a157fbce37374e431c57ec43ec1
kafkaesc <jared.a.hettinger@gmail.com>
2023-05-29 20:01:39 -0500

    Adding custom animations to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,26 @@ module.exports = {
 			red: '#f00',
 			white: '#fff',
 		},
-		extend: {},
+		extend: {
+			keyframes: {
+				'my-spin': {
+					from: { transform: 'rotate(0deg)' },
+					to: { transform: 'rotate(360deg)' },
+				},
+				wiggle: {
+					'0, 100%': { transform: 'rotate(-15deg)' },
+					'50%': { transform: 'rotate(15deg)' },
+				},
+			},
+			animation: {
+				'spin-2': 'my-spin 2s linear infinite',
+				'spin-3': 'my-spin 3s linear infinite',
+				'spin-4': 'my-spin 3s linear infinite',
+				'wiggle-2': 'wiggle 2s ease-in-out infinite',
+				'wiggle-3': 'wiggle 3s ease-in-out infinite',
+				'wiggle-4': 'wiggle 3s ease-in-out infinite',
+			},
+		},
 	},
 	plugins: [],
 };
