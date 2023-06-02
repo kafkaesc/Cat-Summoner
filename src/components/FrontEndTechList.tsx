0f2fed6c8c62b805a9e4b4342c9386ef54d05c3e
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-02 15:49:26 -0500

    Adding FrontEndTechList component

---
+++ src/components/FrontEndTechList.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+
+import ReactLogo from 'public/assets/images/tech/react.svg';
+import NextJsLogo from 'public/assets/images/tech/next.svg';
+import TypeScriptLogo from 'public/assets/images/tech/typescript.svg';
+import TailwindLogo from 'public/assets/images/tech/tailwind.svg';
+
+const techLinks = [
+	{
+		alt: 'Next.js Logo',
+		name: 'Next.js',
+		src: NextJsLogo,
+		url: 'https://nextjs.org/',
+	},
+	{
+		alt: 'React Logo',
+		name: 'React',
+		src: ReactLogo,
+		url: 'https://react.dev/',
+	},
+	{
+		alt: 'TypeScript Logo',
+		name: 'TypeScript',
+		src: TypeScriptLogo,
+		url: 'https://www.typescriptlang.org/',
+	},
+	{
+		alt: 'Tailwind Logo',
+		name: 'Tailwind',
+		src: TailwindLogo,
+		url: 'https://tailwindcss.com/',
+	},
+];
+
+export default function FrontEndTechList() {
+	return (
+		<ul className="text-center">
+			{techLinks.map((te) => {
+				return (
+					<li className="inline-block p-3 mx-3" key={`${te.name}-Logo`}>
+						<a href={te.url} rel="noreferrer" target="_blank">
+							<Image alt={te.alt} height={48} src={te.src} />
+						</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
