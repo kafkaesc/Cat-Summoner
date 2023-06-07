550db47c982966303326ef1c033b843161d8f1bf
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-07 12:40:06 -0500

    Adding className as an optional prop for A

--- src/elements/A.tsx
+++ src/elements/A.tsx
@@ -1,10 +1,18 @@
 interface AProps {
 	children: any;
+	className?: string;
 	[props: string]: any;
 }
 
-export default function A({ children, ...props }: AProps) {
-	return (
+export default function A({ children, className, ...props }: AProps) {
+	return className ? (
+		<a
+			{...props}
+			className={`text-link hover:text-link-hover hover:underline ${className}`}
+		>
+			{children}
+		</a>
+	) : (
 		<a {...props} className="text-link hover:text-link-hover hover:underline">
 			{children}
 		</a>
