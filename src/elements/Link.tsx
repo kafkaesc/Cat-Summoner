61db76c9c5fd51a084738836556819adb0e2a353
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-07 12:44:10 -0500

    Adding className as a propr for Link

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -3,11 +3,25 @@ import NextLink from 'next/link';
 interface LinkProps {
 	href: string;
 	children: any;
+	className?: string;
 	[props: string]: any;
 }
 
-export default function Link({ href, children, ...props }: LinkProps) {
-	return (
+export default function Link({
+	href,
+	children,
+	className,
+	...props
+}: LinkProps) {
+	return className ? (
+		<NextLink
+			{...props}
+			className={`text-link hover:text-link-hover hover:underline ${className}`}
+			href={href}
+		>
+			{children}
+		</NextLink>
+	) : (
 		<NextLink
 			{...props}
 			className="text-link hover:text-link-hover hover:underline"
