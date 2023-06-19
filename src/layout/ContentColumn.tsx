45a9cfa1fce4324a3a4c595aea51558da6674dd0
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-19 12:39:39 -0500

    Adding padding to the ContentColumn

--- src/layout/ContentColumn.tsx
+++ src/layout/ContentColumn.tsx
@@ -1,3 +1,3 @@
 export default function ContentColumn({ children }: any) {
-	return <div className="max-w-4xl mx-auto">{children}</div>;
+	return <div className="max-w-4xl px-2 mx-auto">{children}</div>;
 }
