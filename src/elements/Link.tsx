c9c357b376d23e127561976617aa005565b7793d
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-03 15:38:45 -0500

    Adding linkStyle argument to Link component

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -1,19 +1,37 @@
 import NextLink from 'next/link';
 
 interface LinkProps {
-	href: string;
 	children: any;
 	className?: string;
+	href: string;
+	linkStyle?: string;
 	[props: string]: any;
 }
 
 export default function Link({
-	href,
 	children,
 	className,
+	href,
+	linkStyle,
 	...props
 }: LinkProps) {
-	return className ? (
+	return linkStyle === 'dark-background' && className ? (
+		<NextLink
+			{...props}
+			className={`text-link-dark-bg hover:text-link-dark-bg-hover hover:underline ${className}`}
+			href={href}
+		>
+			{children}
+		</NextLink>
+	) : linkStyle === 'dark-background' ? (
+		<NextLink
+			{...props}
+			className="text-link-dark-bg hover:text-link-dark-bg-hover hover:underline"
+			href={href}
+		>
+			{children}
+		</NextLink>
+	) : className ? (
 		<NextLink
 			{...props}
 			className={`text-link hover:text-link-hover hover:underline ${className}`}
