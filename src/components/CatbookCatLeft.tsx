26677545b5b931d9d3b3e04f337ea25bd8fbfd03
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-03 13:14:44 -0500

    Adding l/r sides for the Catbook page

---
+++ src/components/CatbookCatLeft.tsx
@@ -0,0 +1,7 @@
+export default function CatbookCatLeft({ children }: any) {
+	return (
+		<div className="text-center align-top full-width md:text-right md:inline-block md:w-1/2">
+			{children}
+		</div>
+	);
+}
