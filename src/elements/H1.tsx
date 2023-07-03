f96e9ffd79f053619b765476b542b8a8b6308224
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-03 12:46:22 -0500

    Updating the H1 component to respond to text align classes

--- src/elements/H1.tsx
+++ src/elements/H1.tsx
@@ -5,7 +5,14 @@ interface H1Props {
 }
 
 export default function H1({ children, className, ...props }: H1Props) {
-	return className ? (
+	return className &&
+		(className.includes('text-center') ||
+			className.includes('text-left') ||
+			className?.includes('text-right')) ? (
+		<h1 {...props} className={`text-4xl font-bold ${className}`}>
+			{children}
+		</h1>
+	) : className ? (
 		<h1 {...props} className={`text-4xl font-bold text-center ${className}`}>
 			{children}
 		</h1>
