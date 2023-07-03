b0e6efc02d48179eeb6b0d0f6b2bd126ca10c776
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-03 15:45:38 -0500

    Updating 404 page to not display a cat

--- src/components/Heading404.tsx
+++ src/components/Heading404.tsx
@@ -1,10 +1,8 @@
-import ContentColumn from '@/layout/ContentColumn';
-
 export default function Heading404() {
 	return (
 		<div className="mb-2 text-center">
-			<h1 className="font-bold text-8xl">404</h1>
-			<h2 className="text-3xl font-bold">Cat Not Found</h2>
+			<h1 className="font-bold text-cs-white text-8xl">404</h1>
+			<h2 className="text-3xl font-bold text-cs-white">Cat Not Found</h2>
 		</div>
 	);
 }
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,34 @@
+import Image from 'next/image';
 import Link from '@/elements/Link';
 import P from '@/elements/P';
 import ContentColumn from '@/layout/ContentColumn';
 import Heading404 from '@/components/Heading404';
+import { useCatImageDetails } from '@/hooks/useCatImageDetails';
 
 export default function Custom404() {
+	const { getSpecialImage } = useCatImageDetails();
+	const image = getSpecialImage('404');
 	return (
-		<ContentColumn>
-			<Heading404 />
-			<P className="text-center">
-				That page does not exist. Please double check the URL or{' '}
-				<Link href="/">return home</Link>.
-			</P>
-		</ContentColumn>
+		<div className="h-screen bg-black">
+			<ContentColumn>
+				<Heading404 />
+				{image && (
+					<Image
+						alt={image.alt}
+						className="mx-auto"
+						height={400}
+						width={400}
+						src={image.src}
+					/>
+				)}
+				<P className="text-center text-cs-white">
+					That page does not exist. Please double check the URL or{' '}
+					<Link href="/" linkStyle="dark-background">
+						return home
+					</Link>
+					.
+				</P>
+			</ContentColumn>
+		</div>
 	);
 }
