b217adfde2732c9ec23aee904253bc53c454a97a
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-12 12:36:19 -0500

    Creating cat highlight+images hook, updating catbook cat to use the new hook

--- src/components/CatbookCat.tsx
+++ src/components/CatbookCat.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import H1 from '@/elements/H1';
+import H2 from '@/elements/H2';
 import P from '@/elements/P';
 import Layout from '@/layout/Layout';
 import CatbookCatError from '@/components/CatbookCatError';
 import CatbookCatLeft from '@/components/CatbookCatLeft';
 import CatbookCatRight from '@/components/CatbookCatRight';
-import { useCatImageDetails } from '@/hooks/useCatImageDetails';
+import { useCatbookHighlightAndImages } from '@/hooks/useCatbookHighlightAndImages';
 import { useCats } from '@/hooks/useCats';
 
 interface CatbookCatProps {
@@ -15,11 +16,12 @@ interface CatbookCatProps {
 
 export default function CatbookCat({ catName }: CatbookCatProps) {
 	const { getCatByName } = useCats();
-	const { getImageDetails } = useCatImageDetails();
+	const { getHighlightAndImages } = useCatbookHighlightAndImages();
 	const cat = getCatByName(catName);
-	const image = getImageDetails(catName, 1);
 
 	if (!cat) return <CatbookCatError />;
+
+	const hai = getHighlightAndImages(catName);
 
 	return (
 		<Layout>
@@ -28,21 +30,24 @@ export default function CatbookCat({ catName }: CatbookCatProps) {
 			</Head>
 			<div className="pt-1.5 full-width">
 				<CatbookCatLeft>
-					{image && (
+					{hai && hai.highlight && (
 						<Image
-							alt={image.alt}
+							alt={hai.highlight.alt}
 							className="inline-block"
 							height={500}
 							width={500}
-							src={image.src}
+							src={hai.highlight.src}
 						/>
 					)}
 				</CatbookCatLeft>
 				<CatbookCatRight>
-					<H1 className="text-center md:text-left">{cat.name}</H1>
-					<P>Some details about {cat.name}.</P>
+					<H1 className="mb-2 text-center md:text-left">{cat.name}</H1>
 					{cat.subheadline && <P>{cat.subheadline}</P>}
+					{cat.about && <P>{cat.about}</P>}
 				</CatbookCatRight>
+			</div>
+			<div className="border pt-1.5 full-width">
+				<H2>Pics</H2>
 			</div>
 		</Layout>
 	);
---
+++ src/hooks/useCatbookHighlightAndImages.ts
@@ -0,0 +1,67 @@
+import { useCatbook } from '@/hooks/useCatbook';
+import { useCatImageDetails } from '@/hooks/useCatImageDetails';
+import { CatImageDetails } from '@/interfaces/CatInterfaces';
+
+interface HighlightAndImages {
+	highlight: CatImageDetails | null;
+	images: Array<CatImageDetails> | null;
+}
+
+export function useCatbookHighlightAndImages() {
+	const { getCatbookDataFor } = useCatbook();
+	const { getImageDetails } = useCatImageDetails();
+
+	function getHighlightAndImages(name: string): HighlightAndImages | null {
+		if (
+			name.toLowerCase() !== 'fearless' &&
+			name.toLowerCase() !== 'harvey' &&
+			name.toLowerCase() !== 'lalo' &&
+			name.toLowerCase() !== 'zelda'
+		) {
+			return null;
+		}
+		let catbookList = getCatbookDataFor(name);
+		if (!catbookList || catbookList.length === 0) return null;
+
+		// Shuffle the images available via Catbook, assign the first image as the
+		// highlight image, assign all remaining images to the images attribute.
+		catbookList = shuffle(catbookList);
+		const highlight = getImageDetails(name, catbookList[0]);
+		catbookList.shift();
+		let images: CatImageDetails[] = [];
+		catbookList.map((catImageId) => {
+			const img = getImageDetails(name, catImageId);
+			if (img) images.push(img);
+		});
+
+		/*console.log('name: ', name);
+		console.log('highlight: ', highlight);
+		console.log('images: ', images);*/
+
+		return {
+			highlight: highlight,
+			images: images.length > 0 ? images : null,
+		};
+	}
+
+	function shuffle(arr: Array<any>) {
+		// Move backward through the array and pick random numbers
+		// below the currentIndex to swap with along the way.
+		let currentIndex = arr.length;
+		let randomIndex = 0;
+		while (currentIndex != 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+
+			// Swap them
+			[arr[currentIndex], arr[randomIndex]] = [
+				arr[randomIndex],
+				arr[currentIndex],
+			];
+		}
+
+		return arr;
+	}
+
+	return { getHighlightAndImages };
+}
