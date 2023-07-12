e172b7145e0230e9636408da2fcd97594fff8fe9
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-12 12:32:33 -0500

    Adding more details to the cats

--- src/hooks/useCats.ts
+++ src/hooks/useCats.ts
@@ -1,9 +1,36 @@
 import { Cat } from '@/interfaces/CatInterfaces';
 
 const cats: Array<Cat> = [
-	{ id: 901, name: 'Fearless', subheadline: 'The cat without fear' },
-	{ id: 902, name: 'Harvey' },
-	{ id: 903, name: 'Lalo', subheadline: 'The cow cat' },
+	{
+		about:
+			'Fearless earned her name for jumping on the hood of a car and ' +
+			'meowing through the windshield to make sure she was fed before the ' +
+			'humans left in the morning. She is obsessed with potato chips and ' +
+			'will run over if she hears a bag open. However, she is not a fan of ' +
+			'tortilla chips. If she feels you have tricked her by opening a ' +
+			'bag she will bully you.',
+		id: 901,
+		name: 'Fearless',
+		subheadline: 'The Cat Without Fear',
+	},
+	{
+		about:
+			'Harvey has no respect for your personal boundaries and will jump on ' +
+			'the table to steal any food he can get his paws on. His second ' +
+			'favorite place to sleep is anywhere—his favorite place is on top ' +
+			'of your car.',
+		id: 902,
+		name: 'Harvey',
+	},
+	{
+		about:
+			'Lalo was found underneath a taco truck before arriving at his new ' +
+			"home. He's a very independent cat and loves to sit on the couch and " +
+			'watch tv.',
+		id: 903,
+		name: 'Lalo',
+		subheadline: 'The Cow Cat',
+	},
 	{ id: 904, name: 'Zelda' },
 ];
 
