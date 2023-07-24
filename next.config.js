073188a1fda20c42cf78a0c75903afab6bb08af5
kafkaesc <jared.a.hettinger@gmail.com>
2023-07-24 15:36:11 -0500

    Updating next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+const debug = process.env.NODE_ENV !== 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	exportPathMap: function () {
+		// /Next-React-Components
+		return {
+			'/': { page: '/' },
+		};
+	},
+	assetPrefix: !debug ? 'https://kafkaesc.github.io/Cat-Summoner/' : '',
 	reactStrictMode: true,
 };
 
