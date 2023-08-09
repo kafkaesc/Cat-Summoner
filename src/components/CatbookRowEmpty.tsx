fd950ae57dc3b56aa0f76f6e10e04cfdcb0df516
kafkaesc <jared.a.hettinger@gmail.com>
2023-08-09 18:27:59 -0500

    Adding CatbookRowEmpty component

---
+++ src/components/CatbookRowEmpty.tsx
@@ -0,0 +1,16 @@
+import Link from '@/elements/Link';
+import P from '@/elements/P';
+
+interface CatbookRowEmptyProps {
+	name: string;
+}
+
+export default function EmptyCatbookRow({ name }: CatbookRowEmptyProps) {
+	return (
+		<P className="text-left">
+			You have not unlocked any {name} pictures. You can visit the{' '}
+			<Link href="/">Cat Summoner homepage</Link> to call them a few times, then
+			check back here.
+		</P>
+	);
+}
