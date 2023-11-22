4aefb4a625976692d154f60e25224d0b7407a567
kafkaesc <jared.a.hettinger@gmail.com>
2023-11-21 20:30:01 -0600

    Adding JSDoc for the EmLogo

--- src/layout/EmLogo.tsx
+++ src/layout/EmLogo.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Logo from 'public/logo.png';
 
+/**
+ * @returns {JSX.Element} An image of the site logo a little less than 2rem wide and tall
+ */
 export default function EmLogo() {
 	return <Image alt="Cat-Summoner Logo" src={Logo} className="w-7 h-7" />;
 }
