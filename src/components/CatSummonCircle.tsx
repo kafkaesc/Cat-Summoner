a056f4f313d7c935a421a38432aed6aa2e55db5e
kafkaesc <jared.a.hettinger@gmail.com>
2023-11-28 12:19:57 -0600

    Moving cat summoning UI into a CatSummonCircle component

---
+++ src/components/CatSummonCircle.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import Button from '@/elements/Button';
+import { useCatSummon } from '@/hooks/useCatSummon';
+
+import loading from 'public/assets/images/loading.png';
+
+const cats = ['Fearless', 'Harvey', 'Lalo', 'Zelda'];
+
+export default function CatSummonCircle() {
+	const { isLoading: catIsLoading, summonedCat, summonCat } = useCatSummon();
+
+	return (
+		<div className="text-center md:flex">
+			<div className="w-full mb-1 md:flex-none md:w-32 lg:w-64 md:py-1 md:px-0.5">
+				{cats.map((cat) => (
+					<Button
+						buttonStyle="secondary"
+						className="md:mb-1 md:w-full"
+						key={cat}
+						onClick={() => summonCat(cat)}
+					>
+						{cat}
+					</Button>
+				))}
+			</div>
+			<div className="w-full md:flex-grow md:py-1 md:px-0.5">
+				{catIsLoading && (
+					<Image
+						alt="Loading"
+						className="mx-auto animate-spin-3 md:m-0"
+						height="500"
+						width="500"
+						src={loading}
+					/>
+				)}
+				{catIsLoading && summonedCat && (
+					<Image
+						alt={summonedCat.alt}
+						className="hidden"
+						height={500}
+						width={500}
+						src={summonedCat.src}
+					/>
+				)}
+				{!catIsLoading && summonedCat && (
+					<Image
+						alt={summonedCat.alt}
+						className="mx-auto md:m-0"
+						height={500}
+						width={500}
+						src={summonedCat.src}
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Button from '@/elements/Button';
 import H1 from '@/elements/H1';
 import Layout from '@/layout/Layout';
-import { useCatSummon } from '@/hooks/useCatSummon';
-
-import loading from 'public/assets/images/loading.png';
-
-const cats = ['Fearless', 'Harvey', 'Lalo', 'Zelda'];
+import CatSummonCircle from '@/components/CatSummonCircle';
 
 /**
  * @returns {JSX.Element} JSX component for the index/home page
  */
 export default function Home(): JSX.Element {
-	const { isLoading: catIsLoading, summonedCat, summonCat } = useCatSummon();
-
 	return (
 		<>
 			<Head>
@@ -61,49 +53,7 @@ export default function Home(): JSX.Element {
 			</Head>
 			<Layout>
 				<H1 className="mb-2">Cat Summoner</H1>
-				<div className="text-center md:flex">
-					<div className="w-full mb-1 md:flex-none md:w-32 lg:w-64 md:py-1 md:px-0.5">
-						{cats.map((cat) => (
-							<Button
-								buttonStyle="secondary"
-								className="md:mb-1 md:w-full"
-								key={cat}
-								onClick={() => summonCat(cat)}
-							>
-								{cat}
-							</Button>
-						))}
-					</div>
-					<div className="w-full md:flex-grow md:py-1 md:px-0.5">
-						{catIsLoading && (
-							<Image
-								alt="Loading"
-								className="mx-auto animate-spin-3 md:m-0"
-								height="500"
-								width="500"
-								src={loading}
-							/>
-						)}
-						{catIsLoading && summonedCat && (
-							<Image
-								alt={summonedCat.alt}
-								className="hidden"
-								height={500}
-								width={500}
-								src={summonedCat.src}
-							/>
-						)}
-						{!catIsLoading && summonedCat && (
-							<Image
-								alt={summonedCat.alt}
-								className="mx-auto md:m-0"
-								height={500}
-								width={500}
-								src={summonedCat.src}
-							/>
-						)}
-					</div>
-				</div>
+				<CatSummonCircle />
 			</Layout>
 		</>
 	);
