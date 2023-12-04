7fb02540ea1711df0788996222cb4aacf304888b
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-04 15:02:59 -0600

    Removing hyphens from Cat Summoner

--- src/layout/EmLogo.tsx
+++ src/layout/EmLogo.tsx
@@ -5,5 +5,5 @@ import Logo from 'public/logo.png';
  * @returns {JSX.Element} An image of the site logo a little less than 2rem wide and tall
  */
 export default function EmLogo(): JSX.Element {
-	return <Image alt="Cat-Summoner Logo" src={Logo} className="w-7 h-7" />;
+	return <Image alt="Cat Summoner Logo" src={Logo} className="w-7 h-7" />;
 }
