56a3cafde595361754f2c94c7e249c328710972c
kafkaesc <jared.a.hettinger@gmail.com>
2024-10-22 13:04:04 -0500

    Removing cache breaking and making meta tages one line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,14 +20,8 @@ export default function Home(): JSX.Element {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 				{/* OpenGraph Meta Tags */}
-				<meta
-					property="og:description"
-					content="The Next cat app, built by Jared Hettinger"
-				/>
-				<meta
-					property="og:image"
-					content="https://cat-summoner.com/ogp.png?v=2"
-				/>
+				<meta property="og:description" content="The Next cat app, built by Jared Hettinger" />
+				<meta property="og:image" content="https://cat-summoner.com/ogp.png" />
 				<meta property="og:image:height" content="630" />
 				<meta property="og:image:width" content="1200" />
 				<meta property="og:title" content="Cat Summoner" />
@@ -36,18 +30,9 @@ export default function Home(): JSX.Element {
 				{/* Twitter Card Meta Tags */}
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:creator" content="@_kafkaesc" />
-				<meta
-					name="twitter:description"
-					content="The Next cat app, built by Jared Hettinger"
-				/>
-				<meta
-					name="twitter:image"
-					content="https://cat-summoner.com/twc.png?v=2"
-				/>
-				<meta
-					name="twitter:image:alt"
-					content="A black and white cat sits in front of a dark night sky, he looks back toward the camera, the words Cat Summoner float in the foreground by his face"
-				/>
+				<meta name="twitter:description" content="The Next cat app, built by Jared Hettinger" />
+				<meta name="twitter:image" content="https://cat-summoner.com/twc.png" />
+				<meta name="twitter:image:alt" content="A black and white cat sits in front of a dark night sky, he looks back toward the camera, the words Cat Summoner float in the foreground by his face" />
 				<meta name="twitter:site" content="@_kafkaesc" />
 				<meta name="twitter:title" content="Cat Summoner" />
 			</Head>
