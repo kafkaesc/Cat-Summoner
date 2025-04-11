a98ed53292938fb758f674a09a49ed2b471c55dd
kafkaesc <jared.a.hettinger@gmail.com>
2025-04-11 18:46:33 -0500

    Updating Next. Refactoring image imports.

--- src/layout/EmLogo.tsx
+++ src/layout/EmLogo.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
-import Logo from 'public/logo.png';
+// import Logo from 'public/logo.png';
 
 /**
  * @returns {JSX.Element} An image of the site logo a little less than 2rem wide and tall
  */
 export default function EmLogo(): JSX.Element {
-	return <Image alt="Cat Summoner Logo" src={Logo} className="w-7 h-7" />;
+	return (
+		<Image
+			alt="Cat Summoner Logo"
+			className="w-7 h-7"
+			height={48}
+			src="/logo.png"
+			width={48}
+		/>
+	);
 }
