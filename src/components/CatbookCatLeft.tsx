996b64e4dc06f76bb95b97555ce356be462c6cdb
kafkaesc <jared.a.hettinger@gmail.com>
2026-03-16 23:24:30 -0500

    any to ReactNode updates

--- src/components/CatbookCatLeft.tsx
+++ src/components/CatbookCatLeft.tsx
@@ -1,4 +1,8 @@
-export default function CatbookCatLeft({ children }: any): JSX.Element {
+interface CcLeftProps {
+	children: React.ReactNode;
+}
+
+export default function CatbookCatLeft({ children }: CcLeftProps): JSX.Element {
 	return (
 		<div className="text-center align-top full-width md:text-right md:inline-block md:w-1/2">
 			{children}
