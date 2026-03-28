b1159c4416ed1bb989711c1e809bd9a23c370701
kafkaesc <jared.a.hettinger@gmail.com>
2026-03-27 21:05:13 -0500

    Updating heading hierarchy

--- src/components/Heading404.tsx
+++ src/components/Heading404.tsx
@@ -1,11 +1,9 @@
-/**
- * @returns {JSX.Element} Heading for the 404 page
- */
+/** Heading for the 404 page */
 export default function Heading404() {
 	return (
 		<div className="mb-2 text-center">
-			<h1 className="font-bold text-cs-white text-8xl">404</h1>
-			<h2 className="text-3xl font-bold text-cs-white">Cat Not Found</h2>
+			<h2 className="font-bold text-cs-white text-8xl">404</h2>
+			<h1 className="text-3xl font-bold text-cs-white">Cat Not Found</h1>
 		</div>
 	);
 }
