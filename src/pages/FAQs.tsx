01e69067419c5d7654e221a5a8ef8d08f7b8aeb2
kafkaesc <jared.a.hettinger@gmail.com>
2026-03-27 21:56:51 -0500

    Updating page descriptions

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -15,7 +15,7 @@ export default function Faqs() {
 			<Head>
 				<title>Frequently Asked Questions 🐱 Cat Summoner</title>
 				{/* OpenGraph Meta Tags */}
-				<meta property="og:description" content="FAQs (frequently asked questions) about Cat Summoner" />
+				<meta property="og:description" content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser." />
 				<meta property="og:image" content="https://cat-summoner.com/ogp.png" />
 				<meta property="og:image:height" content="630" />
 				<meta property="og:image:width" content="1200" />
@@ -25,7 +25,7 @@ export default function Faqs() {
 				{/* Twitter Card Meta Tags */}
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:creator" content="@_kafkaesc" />
-				<meta name="twitter:description" content="FAQs (frequently asked questions) about Cat Summoner" />
+				<meta name="twitter:description" content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser." />
 				<meta name="twitter:image" content="https://cat-summoner.com/twc.png" />
 				<meta name="twitter:image:alt" content="A black and white cat sits in front of a dark night sky, he looks back toward the camera, the words Cat Summoner float in the foreground by his face" />
 				<meta name="twitter:site" content="@_kafkaesc" />
