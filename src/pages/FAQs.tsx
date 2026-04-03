2413f2f085f4a36e7d39c91cda957d9f21a851fd
kafkaesc <jared.a.hettinger@gmail.com>
2026-04-02 22:11:24 -0500

    meta tag reverts

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -15,36 +15,21 @@ export default function Faqs() {
 			<Head>
 				<title>Frequently Asked Questions 🐱 Cat Summoner</title>
 				{/* OpenGraph Meta Tags */}
-				<meta
-					property="og:description"
-					content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser."
-				/>
+				<meta property="og:description" content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser." />
 				<meta property="og:image" content="https://cat-summoner.com/ogp.png" />
 				<meta property="og:image:height" content="630" />
 				<meta property="og:image:width" content="1200" />
-				<meta
-					property="og:title"
-					content="FAQs 🐱 Cat Summoner, the Next cat app"
-				/>
+				<meta property="og:title" content="FAQs 🐱 Cat Summoner, the Next cat app" />
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content="https://cat-summoner.com/FAQs" />
 				{/* Twitter Card Meta Tags */}
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:creator" content="@_kafkaesc" />
-				<meta
-					name="twitter:description"
-					content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser."
-				/>
+				<meta name="twitter:description" content="FAQs (frequently asked questions) about Cat Summoner. Make new friends. Call a cat by name, wait a few seconds, and see a photo of them appear. Collect new photos in your Catbook, stored locally in your browser." />
 				<meta name="twitter:image" content="https://cat-summoner.com/twc.png" />
-				<meta
-					name="twitter:image:alt"
-					content="A black and white cat sits in front of a dark night sky, he looks back toward the camera, the words Cat Summoner float in the foreground by his face"
-				/>
+				<meta name="twitter:image:alt" content="A black and white cat sits in front of a dark night sky, he looks back toward the camera, the words Cat Summoner float in the foreground by his face" />
 				<meta name="twitter:site" content="@_kafkaesc" />
-				<meta
-					name="twitter:title"
-					content="FAQs 🐱 Cat Summoner, the Next cat app"
-				/>
+				<meta name="twitter:title" content="FAQs 🐱 Cat Summoner, the Next cat app" />
 			</Head>
 			<H1 className="mb-0">FAQs</H1>
 			<span className="block mb-2 font-serif text-lg italic text-center">
