1c3f6228238170751b72b0b4fabb167e1a0f2454
kafkaesc <jared.a.hettinger@gmail.com>
2026-07-01 08:19:27 -0500

    Using the main element on the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -33,7 +33,7 @@ export default function Custom404() {
 				<meta name="twitter:site" content="@_kafkaesc" />
 				<meta name="twitter:title" content="404: Page Not Found 🐱 Cat Summoner" />
 			</Head>
-			<div className="h-screen bg-black">
+			<main className="h-screen bg-black">
 				<ContentColumn>
 					<Heading404 />
 					{image && (
@@ -53,7 +53,7 @@ export default function Custom404() {
 						.
 					</P>
 				</ContentColumn>
-			</div>
+			</main>
 		</>
 	);
 }
