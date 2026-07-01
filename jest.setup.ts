85dcb0813f8a0ec8cfe7911df3131faea4e59595
kafkaesc <jared.a.hettinger@gmail.com>
2026-07-01 07:45:35 -0500

    Adding a next/router mock to jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom';
+
+// Default next/router mock so components that call useRouter, e.g., NavItem,
+// render in tests. Suites that assert routing behavior override this one.
+jest.mock('next/router', () => ({
+	useRouter: () => ({ pathname: '/' }),
+}));
